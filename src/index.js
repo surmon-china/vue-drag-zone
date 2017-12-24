848b657910f7ac4cd7b122777f41e5dd904e37aa
surmon <mcc@meitu.com>
2017-12-24 22:40:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import dragZone from './zone.vue'
 import dragHandle from './handle.vue'
 import dragContent from './content.vue'
 
-const install = Vue => {
+const install = function (Vue) {
   Vue.component(dragZone.name, dragZone)
   Vue.component(dragHandle.name, dragHandle)
   Vue.component(dragContent.name, dragContent)
 }
-const VueDragZone = { dragZone, dragHandle, dragContent, install }
+const VueDragZone = { dragZone, dragHandle, dragContent, install };
 
 export default VueDragZone
 export { dragZone, dragHandle, dragContent, install }
